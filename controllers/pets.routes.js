46a83234a92da9175a8e044a77daf76515ddeec0
omarakamal <omaraakamal@gmail.com>
2026-02-16 19:58:37 +0300

    update

--- controllers/pets.routes.js
+++ controllers/pets.routes.js
@@ -14,6 +14,35 @@ router.post('/',async(req,res)=>{
 })
 
 
+router.get('/', async (req,res)=>{
+    try{
+        const foundPets = await Pet.find()
+        res.status(200).json(foundPets)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({err:err.message})
+    }
+})
+
+router.get('/:petId', async (req,res)=>{
+    try{
+        const foundPet = await Pet.findById(req.params.petId)
+        if(!foundPet){
+            res.status(404)
+            throw new Error('Pet Not Found with Id')
+        }
+        res.status(200).json(foundPet)
+    }
+    catch(err){
+        if(err.statusCode===404){
+            return res.status(404).json({error:err.message})
+        }
+        console.log(err)
+        res.status(500).json({err:err.message})
+    }
+})
+
 
 
 module.exports = router
