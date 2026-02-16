3a22712f539bbb7509aecaa70ff626ea98edf265
omarakamal <omaraakamal@gmail.com>
2026-02-16 19:14:19 +0300

    update

--- controllers/pets.routes.js
+++ controllers/pets.routes.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 const Pet = require('../models/pet')
 
 router.post('/',async(req,res)=>{
+    try{
     const createdPet = await Pet.create(req.body)
+    res.status(201).json(createdPet)
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({err:err.message})
+    }
 })
 
 
